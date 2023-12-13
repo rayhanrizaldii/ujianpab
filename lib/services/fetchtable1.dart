92d80547a859f4a3e728b6345ae996676d7052ca
Rayhan Rizaldi Hi Hukum <rayhanrizaldi.rr@gmail.com>
2023-12-14 00:00:40 +0700

    Update fetchtable1.dart

--- lib/services/fetchtable1.dart
+++ lib/services/fetchtable1.dart
@@ -4,31 +4,34 @@ import 'package:intl/intl.dart';
 import 'dart:html' as html;
 
 String decodeFileName(String encodedFileName) {
-  String decodedFileName = utf8.decode(base64.decode(encodedFileName));
-  return decodedFileName;
+  try {
+    return utf8.decode(base64.decode(encodedFileName));
+  } catch (e) {
+    print('Error decoding file name: $e');
+    return ''; // Handle the error or return a default value
+  }
 }
 
 class FetchTable1 {
-  static Future<Map<String, dynamic>> fetchDataById(int id) async {
-    final response = await http.get(
-        Uri.parse('http://127.0.0.1:81/ujianpab/read_table1_by_id.php?id=$id'));
+  static Future<Map<dynamic, dynamic>> fetchDataById(int id) async {
+    try {
+      final response = await http.get(
+        Uri.parse('http://localhost:81/ujianpab/read_table1_by_id.php?id=$id'),
+      );
 
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-      if (jsonData is Map && jsonData.containsKey('data')) {
-        final data = Map<String, dynamic>.from(jsonData['data']);
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body) as Map<String, dynamic>;
 
-        // Mendekode nama file yang dienkripsi
         if (data.containsKey('foto')) {
           data['foto'] = decodeFileName(data['foto']);
         }
 
         return data;
       } else {
-        throw Exception('Invalid JSON format');
+        throw Exception('Failed to fetch data: ${response.statusCode}');
       }
-    } else {
-      throw Exception('Gagal mengambil data: ${response.statusCode}');
+    } catch (e) {
+      throw Exception('Error fetching data: $e');
     }
   }
 
@@ -135,43 +138,31 @@ Future<void> updateData(
   String gender,
   String alamat,
   String hobi,
-  html.File imagePath,
+  html.File? image,
 ) async {
-  var url = Uri.parse('http://localhost:81/ujianpab/update_table1.php');
+  try {
+    final response = await http.put(
+      Uri.parse('http://127.0.0.1:81/ujianpab/update_table1.php?id=$id'),
+      body: {
+        'nama': nama,
+        'tanggal_lahir': tanggalLahir.toString(),
+        'umur': umur.toString(),
+        'gender': gender,
+        'alamat': alamat,
+        'hobi': hobi,
+        'foto': image != null ? image.toString() : '',
+      },
+    );
 
-  var request = http.MultipartRequest('POST', url);
-
-  request.fields['id'] = id.toString();
-  request.fields['nama'] = nama;
-  request.fields['tanggal_lahir'] =
-      DateFormat('dd-MM-yyyy').format(tanggalLahir);
-  request.fields['umur'] = umur.toString();
-  request.fields['gender'] = gender;
-  request.fields['alamat'] = alamat;
-  request.fields['hobi'] = hobi;
-
-  // ignore: unnecessary_null_comparison
-  if (imagePath != null) {
-    var fileReader = html.FileReader();
-    fileReader.readAsArrayBuffer(imagePath);
-
-    await fileReader.onLoad.first;
-
-    var fileBytes = fileReader.result as List<int>;
-
-    request.files.add(http.MultipartFile.fromBytes('foto', fileBytes,
-        filename: imagePath.name));
-
-    try {
-      var response = await request.send();
-      if (response.statusCode == 200) {
-        print('Data dan gambar berhasil diperbarui.');
-      } else {
-        print(
-            'Gagal memperbarui data dan gambar. Status code: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Terjadi kesalahan saat mengirim permintaan: $e');
+    if (response.statusCode == 200) {
+      // Data updated successfully
+      print('Data updated successfully');
+    } else {
+      // Failed to update data
+      print('Failed to update data: ${response.body}');
     }
+  } catch (e) {
+    // Handle errors
+    print('Error updating data: $e');
   }
 }
